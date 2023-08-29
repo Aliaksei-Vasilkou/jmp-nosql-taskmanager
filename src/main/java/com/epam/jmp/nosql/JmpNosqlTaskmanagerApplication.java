e49e271d2d070536d261d95eac4b58b1eac42643
Aliaksei Vasilkou <Aliaksei.Vasilkou@ihg.com>
2023-08-29 18:49:35 +0200

    initial commit

---
+++ src/main/java/com/epam/jmp/nosql/JmpNosqlTaskmanagerApplication.java
@@ -0,0 +1,13 @@
+package com.epam.jmp.nosql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmpNosqlTaskmanagerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JmpNosqlTaskmanagerApplication.class, args);
+    }
+
+}
